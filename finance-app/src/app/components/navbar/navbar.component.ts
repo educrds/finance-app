d3ccdf49e3c49914fa34647eed8c7fe1174ed3e8
eduardo <eduardocardoso1039@gmail.com>
2024-03-29 17:24:38 -0300

    Refactor | NavbarComponent - Adicionado logo do app & input de seleção do mês.

--- finance-app/src/app/components/navbar/navbar.component.ts
+++ finance-app/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'fin-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  protected monthInputForm!: FormGroup;
 
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.monthInputForm = new FormGroup({
+      month_selected: this._fb.control(this.getCurrentMonthFormatted()),
+    });
+  }
+
+  private getCurrentMonthFormatted() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+
+    const current_month = new Date(`${year}, ${month}, 01`);
+
+    return current_month;
+  }
 }
