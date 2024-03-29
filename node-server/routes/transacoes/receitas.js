01b3b641225118d661174c65c6fe5e648ff77342
eduardo <eduardocardoso1039@gmail.com>
2024-03-29 20:51:06 -0300

    REST API | Receitas - Configurado endpoint e query.

---
+++ node-server/routes/transacoes/receitas.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import executeQuery from '../../db.config.js';
+import { receitas_com_relacionamentos } from './queries.js';
+const router = express.Router();
+
+// Obter transações por usuário
+async function getReceitas() {
+  return await executeQuery(receitas_com_relacionamentos); 
+  }
+
+// chamada GET para obter as transações por usuário
+router.post('/transacoes/listar/receitas', async (req, res) => {
+  const result = await getReceitas();
+  res.send(result);
+});
+
+export default router;
