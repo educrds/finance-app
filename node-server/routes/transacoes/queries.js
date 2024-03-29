3771d70378fa96bc74156265cb28d2aa85d3016b
eduardo <eduardocardoso1039@gmail.com>
2024-03-29 20:06:14 -0300

    REST API | Transações - Configurado endpoint e query.

---
+++ node-server/routes/transacoes/queries.js
@@ -0,0 +1,19 @@
+export const transacoes_com_relacionamentos = `
+  SELECT
+    trs.trs_data_ocorrido,
+    trs.trs_ano_ocorrido,
+    trs.trs_mes_ocorrido,
+    trs.trs_valor,
+    trs.trs_titulo,
+    cat.cat_nome AS categoria_nome,
+    met.met_nome AS metodo,
+    usr.usr_nome AS nome_usuario,
+    tip_trs.tip_nome AS tipo_transacao,
+    tip_trs.tip_id AS id_tipo_transacao,
+    trs.trs_parcelado
+  FROM
+    tb_transacoes trs
+    LEFT JOIN tb_categorias cat ON cat.cat_id = trs.trs_categoria
+    LEFT JOIN tb_metodo met ON met.met_id = trs.trs_metodo
+    LEFT JOIN tb_usuarios usr ON usr.usr_id = trs.trs_usuario
+    LEFT JOIN tb_tipo_transacao tip_trs ON tip_trs.tip_id = trs.trs_tipo`;
--- node-server/routes/transacoes/transacoes.js
+++ node-server/routes/transacoes/transacoes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import executeQuery from '../../db.config.js';
+import { transacoes_com_relacionamentos } from './queries.js';
 const router = express.Router();
 
 // Obter transações por usuário
 async function getTransacoes() {
-  return await executeQuery("SELECT * FROM tb_usuarios WHERE usr_id = ?", 1); 
+  return await executeQuery(transacoes_com_relacionamentos); 
   }
 
 // chamada GET para obter as transações por usuário
 router.post('/transacoes/listar', async (req, res) => {
-
-console.log(req.body);
+// console.log(req.body);
   const result = await getTransacoes();
   res.send(result);
 });
