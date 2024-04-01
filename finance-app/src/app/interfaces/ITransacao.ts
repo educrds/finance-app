7b19010ec0ec2ada11ec0d9ef1e0ad457b63be07
eduardo <eduardocardoso1039@gmail.com>
2024-03-31 21:08:32 -0300

    Refactor | InterfaceTransacao - Adicionado tipo de cor na interface de categoria.

--- finance-app/src/app/interfaces/ITransacao.ts
+++ finance-app/src/app/interfaces/ITransacao.ts
@@ -5,10 +5,11 @@ export interface ITransacao {
   trs_valor: number;
   trs_titulo: string;
   categoria_nome: string;
+  id_categoria: number;
+  categoria_cor: string;
   metodo: string;
   nome_usuario: string;
   tipo_transacao: string;
   id_tipo_transacao: number;
   trs_parcelado: number;
-  categoria_cor: string;
 }
