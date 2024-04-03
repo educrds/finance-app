866666ca6922e429adee0e883c976f99cace1259
eduardo <eduardocardoso1039@gmail.com>
2024-04-03 20:39:20 -0300

    Refactor | ReceitasComponent - Adicionar metódo para checar mudanças vindo do CRUD.

--- finance-app/src/app/components/receitas/receitas.component.ts
+++ finance-app/src/app/components/receitas/receitas.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ITransacao } from '../../interfaces/ITransacao';
 import { TransacoesService } from '../../services/transacoes.service';
 import { take } from 'rxjs';
+import { DateFilterService } from '../../services/date-filter.service';
 
 @Component({
   selector: 'fin-receitas',
   templateUrl: './receitas.component.html',
   styleUrl: './receitas.component.scss'
 })
-export class ReceitasComponent {
+export class ReceitasComponent implements OnInit {
   protected transacoes: ITransacao[] = [];
+  private queryParams: Date = new Date();
 
-  constructor(private _transacoesService: TransacoesService) {}
+  constructor(private _transacoesService: TransacoesService, private _dateFilterService: DateFilterService) {}
 
   ngOnInit(): void {
+    this.getReceitas();
+
+    this._dateFilterService.notifyObservable$.subscribe((res) => {
+      const { date } = res;
+      if (date) {
+        this.queryParams = date;
+        this.getReceitas();
+      }
+    });
+  }
+  
+  
+  private getReceitas(){
     this._transacoesService
-      .getReceitas()
+      .getReceitas(this.queryParams)
       .pipe(take(1))
       .subscribe({
         next: (res) => (this.transacoes = res),
