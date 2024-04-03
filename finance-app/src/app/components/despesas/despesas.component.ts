0f894a0d0e5a2532b424ad5b782445a595468e49
eduardo <eduardocardoso1039@gmail.com>
2024-04-03 20:38:43 -0300

    Refactor | DespesasComponent - Adicionar metódo para checar mudanças vindo do CRUD.

--- finance-app/src/app/components/despesas/despesas.component.ts
+++ finance-app/src/app/components/despesas/despesas.component.ts
@@ -2,20 +2,36 @@ import { Component } from '@angular/core';
 import { TransacoesService } from '../../services/transacoes.service';
 import { ITransacao } from '../../interfaces/ITransacao';
 import { take } from 'rxjs';
+import { DateFilterService } from '../../services/date-filter.service';
 
 @Component({
   selector: 'fin-despesas',
   templateUrl: './despesas.component.html',
-  styleUrl: './despesas.component.scss'
+  styleUrl: './despesas.component.scss',
 })
 export class DespesasComponent {
   protected transacoes: ITransacao[] = [];
+  private queryParams: Date = new Date();
 
-  constructor(private _transacoesService: TransacoesService) {}
+  constructor(
+    private _transacoesService: TransacoesService,
+    private _dateFilterService: DateFilterService
+  ) {}
 
   ngOnInit(): void {
+    this.getDespesas();
+    this._dateFilterService.notifyObservable$.subscribe((res) => {
+      const { date } = res;
+      if (date) {
+        this.queryParams = date;
+        this.getDespesas();
+      }
+    });
+  }
+
+  private getDespesas() {
     this._transacoesService
-      .getDespesas()
+      .getDespesas(this.queryParams)
       .pipe(take(1))
       .subscribe({
         next: (res) => (this.transacoes = res),
