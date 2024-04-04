0fbab58731199fdae17f6138a5dfd926c68e003f
eduardo <eduardocardoso1039@gmail.com>
2024-04-04 20:56:31 -0300

    Feature | DespesasComponent - Adicionado metódos de criar e deletar uma despesa.

--- finance-app/src/app/components/despesas/despesas.component.ts
+++ finance-app/src/app/components/despesas/despesas.component.ts
@@ -3,6 +3,7 @@ import { TransacoesService } from '../../services/transacoes.service';
 import { ITransacao } from '../../interfaces/ITransacao';
 import { take } from 'rxjs';
 import { DateFilterService } from '../../services/date-filter.service';
+import { TransacaoUtilService } from '../../utils/transacao-util.service';
 
 @Component({
   selector: 'fin-despesas',
@@ -11,15 +12,28 @@ import { DateFilterService } from '../../services/date-filter.service';
 })
 export class DespesasComponent {
   protected transacoes: ITransacao[] = [];
+  protected rowSelected!: ITransacao | null;
+
   private queryParams: Date = new Date();
 
   constructor(
     private _transacoesService: TransacoesService,
-    private _dateFilterService: DateFilterService
+    private _dateFilterService: DateFilterService,
+    private _transacaoUtilService: TransacaoUtilService
   ) {}
 
   ngOnInit(): void {
     this.getDespesas();
+
+    this._transacoesService.notifyObservable$.subscribe((res) => {
+      if (res.refresh) {
+        this.getDespesas();
+      }
+      if (res.closeModal) {
+        this.rowSelected = null;
+      }
+    });
+
     this._dateFilterService.notifyObservable$.subscribe((res) => {
       const { date } = res;
       if (date) {
@@ -27,6 +41,18 @@ export class DespesasComponent {
         this.getDespesas();
       }
     });
+  }
+
+  protected editarTransacao(transacao: ITransacao) {
+    this._transacaoUtilService.editarTransacaoUtil(transacao);
+  }
+
+  protected deletarTransacao(idTransacao: number) {
+    this._transacaoUtilService.deletarTransacaoUtil(idTransacao);
+  }
+
+  protected checkStatus(transacao: ITransacao): string | undefined {
+    return this._transacaoUtilService.checkStatusUtil(transacao);
   }
 
   private getDespesas() {
