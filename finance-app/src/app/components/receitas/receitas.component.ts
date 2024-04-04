5fa0b73c12bbe9c4fee59ababda47903554a2599
eduardo <eduardocardoso1039@gmail.com>
2024-04-04 20:56:48 -0300

    Feature | ReceitasComponent - Adicionado metódos de criar e deletar uma receita.

--- finance-app/src/app/components/receitas/receitas.component.ts
+++ finance-app/src/app/components/receitas/receitas.component.ts
@@ -3,32 +3,60 @@ import { ITransacao } from '../../interfaces/ITransacao';
 import { TransacoesService } from '../../services/transacoes.service';
 import { take } from 'rxjs';
 import { DateFilterService } from '../../services/date-filter.service';
+import { TransacaoUtilService } from '../../utils/transacao-util.service';
 
 @Component({
   selector: 'fin-receitas',
   templateUrl: './receitas.component.html',
-  styleUrl: './receitas.component.scss'
+  styleUrl: './receitas.component.scss',
 })
 export class ReceitasComponent implements OnInit {
   protected transacoes: ITransacao[] = [];
+  protected rowSelected!: ITransacao | null;
+
   private queryParams: Date = new Date();
 
-  constructor(private _transacoesService: TransacoesService, private _dateFilterService: DateFilterService) {}
+  constructor(
+    private _transacoesService: TransacoesService,
+    private _dateFilterService: DateFilterService,
+    private _transacaoUtilService: TransacaoUtilService
+  ) {}
 
   ngOnInit(): void {
     this.getReceitas();
 
+    this._transacoesService.notifyObservable$.subscribe((res) => {
+      if (res.refresh) {
+        this.getReceitas();
+      }
+      if (res.closeModal) {
+        this.rowSelected = null;
+      }
+    });
+
     this._dateFilterService.notifyObservable$.subscribe((res) => {
       const { date } = res;
+
       if (date) {
         this.queryParams = date;
         this.getReceitas();
       }
     });
   }
-  
-  
-  private getReceitas(){
+
+  protected editarTransacao(transacao: ITransacao) {
+    this._transacaoUtilService.editarTransacaoUtil(transacao);
+  }
+
+  protected deletarTransacao(idTransacao: number) {
+    this._transacaoUtilService.deletarTransacaoUtil(idTransacao);
+  }
+
+  protected checkStatus(transacao: ITransacao): string | undefined {
+    return this._transacaoUtilService.checkStatusUtil(transacao);
+  }
+
+  private getReceitas() {
     this._transacoesService
       .getReceitas(this.queryParams)
       .pipe(take(1))
