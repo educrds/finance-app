9d3b336acd6113bdca6f6b9df40dd5e23b5c8b9f
eduardo <eduardocardoso1039@gmail.com>
2024-04-04 21:14:47 -0300

    REST API | Transacoes - Refatorado retorno da query.

--- node-server/queries/transacoes/GET/index.js
+++ node-server/queries/transacoes/GET/index.js
@@ -2,16 +2,14 @@ export const transacoes_com_relacionamentos = `
   SELECT
     trs.trs_data_ocorrido,
     trs.trs_id,
-    trs.trs_ano_ocorrido,
-    trs.trs_mes_ocorrido,
     trs.trs_valor,
     trs.trs_titulo,
     trs.trs_status,
     cat.cat_nome AS categoria_nome,
     cat.cat_cor AS categoria_cor,
     cat.cat_id AS id_categoria,
-    met.met_nome AS metodo,
-    usr.usr_nome AS nome_usuario,
+    met.met_nome AS metodo_nome,
+    met.met_id AS metodo_id,
     tip_trs.tip_nome AS tipo_transacao,
     tip_trs.tip_id AS id_tipo_transacao,
     trs.trs_parcelado
@@ -21,22 +19,23 @@ export const transacoes_com_relacionamentos = `
     LEFT JOIN tb_metodo met ON met.met_id = trs.trs_metodo
     LEFT JOIN tb_usuarios usr ON usr.usr_id = trs.trs_usuario
     LEFT JOIN tb_tipo_transacao tip_trs ON tip_trs.tip_id = trs.trs_tipo
-  WHERE trs.trs_ano_ocorrido = ? AND trs.trs_mes_ocorrido = ?  
+  WHERE trs.trs_ano_ocorrido = ? AND trs.trs_mes_ocorrido = ? 
+  ORDER BY trs.trs_data_ocorrido DESC 
 `;
 
 export const receitas_com_relacionamentos = `
   SELECT
       trs.trs_data_ocorrido,
-      trs.trs_ano_ocorrido,
       trs.trs_id,
-      trs.trs_mes_ocorrido,
       trs.trs_valor,
       trs.trs_titulo,
       trs.trs_status,
       cat.cat_nome AS categoria_nome,
       cat.cat_cor AS categoria_cor,
-      met.met_nome AS metodo,
-      usr.usr_nome AS nome_usuario,
+      cat.cat_id AS id_categoria,
+      met.met_nome AS metodo_nome,
+      met.met_id AS metodo_id,
+      met.met_id AS trs_metodo,
       tip_trs.tip_nome AS tipo_transacao,
       tip_trs.tip_id AS id_tipo_transacao,
       trs.trs_parcelado
@@ -47,21 +46,21 @@ export const receitas_com_relacionamentos = `
       LEFT JOIN tb_usuarios usr ON usr.usr_id = trs.trs_usuario
       LEFT JOIN tb_tipo_transacao tip_trs ON tip_trs.tip_id = trs.trs_tipo
     WHERE tip_trs.tip_id = 1 AND trs.trs_ano_ocorrido = ? AND trs.trs_mes_ocorrido = ?  
+    ORDER BY trs.trs_data_ocorrido DESC
 `;
 
 export const despesas_com_relacionamentos = `
   SELECT
       trs.trs_data_ocorrido,
-      trs.trs_ano_ocorrido,
       trs.trs_id,
-      trs.trs_mes_ocorrido,
       trs.trs_valor,
       trs.trs_titulo,
       trs.trs_status,
       cat.cat_nome AS categoria_nome,
       cat.cat_cor AS categoria_cor,
-      met.met_nome AS metodo,
-      usr.usr_nome AS nome_usuario,
+      cat.cat_id AS id_categoria,
+      met.met_nome AS metodo_nome,
+      met.met_id AS metodo_id,
       tip_trs.tip_nome AS tipo_transacao,
       tip_trs.tip_id AS id_tipo_transacao,
       trs.trs_parcelado
@@ -72,13 +71,8 @@ export const despesas_com_relacionamentos = `
       LEFT JOIN tb_usuarios usr ON usr.usr_id = trs.trs_usuario
       LEFT JOIN tb_tipo_transacao tip_trs ON tip_trs.tip_id = trs.trs_tipo
     WHERE tip_trs.tip_id = 2 AND trs.trs_ano_ocorrido = ? AND trs.trs_mes_ocorrido = ?  
+    ORDER BY trs.trs_data_ocorrido DESC
 `;
-
-export const soma_transacoes = `
-  SELECT
-    SUM(CASE WHEN trs_tipo = 1 THEN trs_valor ELSE 0 END) AS soma_receitas,
-    SUM(CASE WHEN trs_tipo = 2 THEN trs_valor ELSE 0 END) AS soma_despesas
-  FROM
-    tb_transacoes
-  WHERE trs_ano_ocorrido = ? AND trs_mes_ocorrido = ?;
+export const get_metodos = `
+  SELECT met_id AS id, met_nome AS text FROM tb_metodo;
 `;
