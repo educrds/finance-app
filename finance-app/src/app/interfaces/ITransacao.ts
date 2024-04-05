9cabf62b43a8314dcad99d8fae861065fe4bafd2
eduardo <eduardocardoso1039@gmail.com>
2024-04-04 21:06:51 -0300

    Refactor | ITransacao - Adicionado valores que estavam faltando na tipagem.

--- finance-app/src/app/interfaces/ITransacao.ts
+++ finance-app/src/app/interfaces/ITransacao.ts
@@ -1,14 +1,15 @@
 export interface ITransacao {
   trs_data_ocorrido: Date;
-  trs_ano_ocorrido: number;
-  trs_mes_ocorrido: number;
   trs_valor: number;
   trs_titulo: string;
+  trs_tipo: number;
+  trs_id: number;
+  trs_status: number;
   categoria_nome: string;
   id_categoria: number;
   categoria_cor: string;
-  metodo: string;
-  nome_usuario: string;
+  metodo_nome: string;
+  metodo_id: number;
   tipo_transacao: string;
   id_tipo_transacao: number;
   trs_parcelado: number;
