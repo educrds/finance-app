67c4f39cbfef9c941e7d8ec1a29d0341d5a8e618
eduardo <eduardocardoso1039@gmail.com>
2024-04-08 12:06:40 -0300

    REST API | Transacoes - Adicionado novo parametro no insert.

--- node-server/queries/transacoes/INSERT/index.js
+++ node-server/queries/transacoes/INSERT/index.js
@@ -1,3 +1,3 @@
 export const insert_transacao = `
-INSERT INTO tb_transacoes (trs_valor, trs_data_ocorrido, trs_titulo, trs_descricao, trs_categoria, trs_usuario, trs_tipo, trs_metodo
-) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
+INSERT INTO tb_transacoes (trs_valor, trs_data_ocorrido, trs_titulo, trs_descricao, trs_categoria, trs_usuario, trs_tipo, trs_metodo, trs_parcelado
+) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
