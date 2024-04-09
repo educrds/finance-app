4901d400af60c81a97877cae6bf3a783e00a5a3e
eduardo <eduardocardoso1039@gmail.com>
2024-04-09 18:58:21 -0300

    Refactor | AppRouting - Configurado novas rotas de autenticação.

--- finance-app/src/app/app-routing.module.ts
+++ finance-app/src/app/app-routing.module.ts
@@ -7,18 +7,32 @@ import { MainComponent } from './components/main/main.component';
 import { ReceitasComponent } from './components/receitas/receitas.component';
 import { DespesasComponent } from './components/despesas/despesas.component';
 import { CategoriasComponent } from './components/categorias/categorias.component';
+import { LoginComponent } from './authenticator/pages/login/login.component';
+import { authGuard } from './guards/auth.guard';
+import { RegisterComponent } from './authenticator/pages/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [authGuard],
     children: [
-      { path: 'all', component:  MainComponent},
-      { path: 'receitas', component:  ReceitasComponent},
-      { path: 'despesas', component:  DespesasComponent},
-      { path: 'categorias', component:  CategoriasComponent},
-      { path: '',   redirectTo: '/all', pathMatch: 'full' },
-    ]
+      { path: 'all', component: MainComponent },
+      { path: 'receitas', component: ReceitasComponent },
+      { path: 'despesas', component: DespesasComponent },
+      { path: 'categorias', component: CategoriasComponent },
+      { path: '', redirectTo: '/all', pathMatch: 'full' },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    children: [],
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    children: [],
   },
 ];
 
