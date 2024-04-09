a705df11c0af40bc132aebbdf1ab260df0631ab0
eduardo <eduardocardoso1039@gmail.com>
2024-04-09 18:56:23 -0300

    Feature | AuthService - Criado interface e metódos de autenticação.

---
+++ finance-app/src/app/authenticator/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable, retry } from 'rxjs';
+import { IUsuario } from '../../interfaces/IUsuario';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private _api_url = environment.api_url;
+
+  constructor(private _http: HttpClient) {}
+
+  authenticateUser(form: IUsuario): Observable<any> {
+    return this._http
+      .post(`${this._api_url}user/login`, { data: form })
+      .pipe(retry(1));
+  }
+
+  registerUser(form: IUsuario): Observable<any> {
+    return this._http
+      .post(`${this._api_url}user/register`, { data: form })
+      .pipe(retry(1));
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+}
---
+++ finance-app/src/app/interfaces/IUsuario.ts
@@ -0,0 +1,5 @@
+export interface IUsuario {
+  auth_name?: string;
+  auth_login: string;
+  auth_password: string;
+}
