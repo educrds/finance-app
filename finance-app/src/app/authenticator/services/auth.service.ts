26ebda03a737a8a0ac1239699bcff77d1159bba0
eduardo <eduardocardoso1039@gmail.com>
2024-04-14 21:34:11 -0300

    Refactor | HttpInterceptor - Adicionado tratamento de erros com base em status em cada requisição.

--- finance-app/src/app/authenticator/services/auth.service.ts
+++ finance-app/src/app/authenticator/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Observable, retry } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { IUsuario } from '../../interfaces/IUsuario';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -10,18 +11,32 @@ import { IUsuario } from '../../interfaces/IUsuario';
 export class AuthService {
   private _api_url = environment.api_url;
 
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient, private _router: Router) {}
 
   authenticateUser(form: IUsuario): Observable<any> {
-    return this._http
-      .post(`${this._api_url}user/login`, { data: form })
-      .pipe(retry(1));
+    return this._http.post(`${this._api_url}user/login`, { data: form })
   }
 
   registerUser(form: IUsuario): Observable<any> {
-    return this._http
-      .post(`${this._api_url}user/register`, { data: form })
-      .pipe(retry(1));
+    return this._http.post(`${this._api_url}user/register`, { data: form })
   }
 
+  handleHttpError(error: HttpErrorResponse) {
+    // return catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        // Se a resposta for 401 Unauthorized, redirecione para a rota /login
+        this._router.navigate(['/login']);
+      } else if (error.status === 404) {
+        // Trate o erro 404 (Not Found) de acordo com os requisitos da sua aplicação
+        console.error('Recurso não encontrado. Status: 404');
+      } else if (error.status === 500) {
+        // Trate o erro 500 (Internal Server Error) de acordo com os requisitos da sua aplicação
+        console.error('Erro interno do servidor. Status: 500');
+      } else {
+        // Trate outros erros de acordo com os requisitos da sua aplicação
+        console.error('Erro não tratado. Status:', error.status);
+      }
+      return throwError(error);
+    // });
+  }
 }
--- finance-app/src/app/helpers/http-interceptor.interceptor.ts
+++ finance-app/src/app/helpers/http-interceptor.interceptor.ts
@@ -9,46 +9,54 @@ import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { StorageService } from '../shared/services/storage.service';
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { AuthService } from '../authenticator/services/auth.service';
+import { MessagesService } from '../services/messages.service';
 
 @Injectable()
 export class ApiRequestInterceptor implements HttpInterceptor {
   constructor(
     private _storageService: StorageService,
-    private _router: Router
+    private _router: Router,
+    private _messagesService: MessagesService,
+    private _authService: AuthService
   ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    if (req.url.includes('/login') || req.url.includes('/register')) {
-      return next.handle(req);
+    const authRoutes = req.url.includes('/login') || req.url.includes('/register');
+
+    if (authRoutes) {
+      return next.handle(req).pipe(
+        catchError((err: HttpErrorResponse) => {
+          this._messagesService.showError(err.error.message);
+          return throwError(err);
+        })
+      );
     }
 
+    // Verifica usuario logado e segue com a requisiçao enviando payload modificado
+    // com headers e id do usuário no body.
     if (this._storageService.isLoggedIn()) {
       const token = this._storageService.getToken;
-      const decoded = token && jwtDecode<JwtPayload>(token);
 
       req = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`,
         },
-        body: { ...req.body, usr_id: decoded?.sub },
       });
 
-      return next.handle(req).pipe(
-        catchError((error: HttpErrorResponse) => {
-          if (error.status === 401) {
-            // Se a resposta for 401 Unauthorized, redirecione para a rota /all
-            this._router.navigate(['/login']);
-          }
-          return throwError(error);
-        })
-      );
-    } else {
-      this._router.navigate(['/login']);
-      return throwError('Usuário não autenticado');
+      return next
+        .handle(req)
+        .pipe(
+          catchError((error: HttpErrorResponse) => this._authService.handleHttpError(error))
+        );
     }
+
+    this._router.navigate(['/login']);
+    this._messagesService.showError('Usúario não autenticado.');
+    return throwError('Usuário não autenticado');
   }
 }
+
 export const httpInterceptorProvider = {
   provide: HTTP_INTERCEPTORS,
   useClass: ApiRequestInterceptor,
