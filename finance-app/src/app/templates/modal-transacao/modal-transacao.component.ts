e3ce1a78687ba9c9f7b2585f75f231d737583208
eduardo <eduardocardoso1039@gmail.com>
2024-04-17 19:59:57 -0300

    Feature | ModalTransacaoComponent - Adicionado validação de campos.

--- finance-app/src/app/templates/modal-transacao/modal-transacao.component.ts
+++ finance-app/src/app/templates/modal-transacao/modal-transacao.component.ts
@@ -33,101 +33,94 @@ export class ModalTransacaoComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    const defaultTransactionValues = {
+      trs_valor: '',
+      trs_data_ocorrido: new Date(),
+      trs_titulo: '',
+      trs_categoria: '',
+      trs_usuario: 1,
+      trs_metodo: '',
+      trs_status: false,
+      trs_parcelado: false,
+      data_fim_repeticao: '',
+    };
+
+    const trs_data = this._config.data.trs_data_ocorrido && new Date(this._config.data.trs_data_ocorrido);
     this.tipoTransacao = this._config.data.id_tipo_transacao;
 
-    if (this._config.data.trs_id) {
-      this.formAddTransacao = this._fb.group({
-        trs_valor: [this._config.data.trs_valor],
-        trs_data_ocorrido: [new Date(this._config.data.trs_data_ocorrido)],
-        trs_titulo: [this._config.data?.trs_titulo, Validators.required],
-        trs_categoria: [this._config.data?.id_categoria],
-        trs_usuario: [1],
-        trs_tipo: [this.tipoTransacao],
-        trs_metodo: [this._config.data.metodo_id],
-        trs_status: [!!this._config.data.trs_status],
-        trs_id: [this._config.data.trs_id],
-        trs_parcelado: [this._config.data.trs_parcelado],
-      });
-    } else {
-      this.formAddTransacao = this._fb.group({
-        trs_valor: [''],
-        trs_data_ocorrido: [new Date()],
-        trs_titulo: ['', Validators.required],
-        trs_categoria: [''],
-        trs_usuario: [1],
-        trs_tipo: [this.tipoTransacao],
-        trs_metodo: [''],
-        trs_status: [false],
-        trs_parcelado: [false],
-        data_fim_repeticao: [''],
-      });
-    }
+    this.formAddTransacao = this._fb.group({
+      trs_valor: [this._config.data.trs_valor || defaultTransactionValues.trs_valor, Validators.required],
+      trs_data_ocorrido: [trs_data || defaultTransactionValues.trs_data_ocorrido],
+      trs_titulo: [this._config.data?.trs_titulo || defaultTransactionValues.trs_titulo, Validators.required],
+      trs_categoria: [this._config.data?.id_categoria || defaultTransactionValues.trs_categoria, Validators.required],
+      trs_usuario: [defaultTransactionValues.trs_usuario],
+      trs_tipo: [this.tipoTransacao],
+      trs_metodo: [this._config.data.metodo_id || defaultTransactionValues.trs_metodo, Validators.required],
+      trs_status: [!!this._config.data.trs_status || defaultTransactionValues.trs_status],
+      trs_id: [this._config.data.trs_id || null],
+      trs_parcelado: [this._config.data.trs_parcelado || defaultTransactionValues.trs_parcelado],
+      data_fim_repeticao: [''],
+    });
 
     this._transacoesService.getMetodosDropdown().subscribe({
-      next: (res) => (this.metodosOptions = res),
-      error: () =>
-        this._messagesService.showError(
-          'Ocorreu um erro ao buscar categorias.'
-        ),
+      next: (res) => this.metodosOptions = res,
+      error: () => this._messagesService.showError('Ocorreu um erro ao buscar categorias.'),
     });
 
     this._categoriasService
       .getCategoriasDropdown(this.tipoTransacao)
       .subscribe({
-        next: (res) => (this.categoriasOptions = res),
-        error: () =>
-          this._messagesService.showError(
-            'Ocorreu um erro ao buscar categorias.'
-          ),
+        next: (res) => this.categoriasOptions = res,
+        error: () => this._messagesService.showError('Ocorreu um erro ao buscar categorias.'),
       });
 
-    this._ref.onClose.subscribe(() =>
-      this._notificationService.notifyChanges({ closeModal: true })
-    );
+    this._ref.onClose.subscribe(() => this._notificationService.notifyChanges({ closeModal: true }));
   }
 
   protected inserirOuAtualizarTransacao() {
-    this.loading = true;
-    const form = this.formAddTransacao.getRawValue();
+    if(this.formAddTransacao.valid){
+      this.loading = true;
+      const form = this.formAddTransacao.getRawValue();
 
-    if (form.trs_id) {
-      this.atualizarTransacao(form);
+      if (form.trs_id) {
+        this.atualizarTransacao(form);
+        return;
+      }
+      this.inserirTransacao(form);
       return;
     }
-    this.inserirTransacao(form);
+    this.updateValidationForm(this.formAddTransacao);
+  }
+  
+  private updateValidationForm(group: FormGroup){
+    group.markAllAsTouched();
+    Object.keys(group.controls).map((key: string)=>{
+      const abstractControl = group.controls[key];
+      abstractControl.updateValueAndValidity();
+    })
   }
 
   private inserirTransacao(form: ITransacao) {
     this._transacoesService.addTransacao(form).subscribe({
       next: () => {
-        this._messagesService.showSuccess(
-          'Transação adicionada com successo!'
-        );
+        this._messagesService.showSuccess('Transação adicionada com successo!');
         this._ref.close();
         this._notificationService.notifyChanges({ refresh: true });
       },
-      error: () =>
-        this._messagesService.showError(
-          'Ocorreu um erro ao adicionar transação.'
-        ),
-      complete: () => (this.loading = false),
+      error: () => this._messagesService.showError('Ocorreu um erro ao adicionar transação.'),
+      complete: () => this.loading = false
     });
   }
 
   private atualizarTransacao(form: ITransacao) {
     this._transacoesService.atualizarTransacao(form).subscribe({
       next: () => {
-        this._messagesService.showSuccess(
-          'Transação atualizada com successo!'
-        );
+        this._messagesService.showSuccess('Transação atualizada com successo!');
         this._ref.close();
         this._notificationService.notifyChanges({ refresh: true });
       },
-      error: () =>
-        this._messagesService.showError(
-          'Ocorreu um erro ao atualizar transação.'
-        ),
-      complete: () => (this.loading = false),
+      error: () => this._messagesService.showError('Ocorreu um erro ao atualizar transação.'),
+      complete: () => this.loading = false
     });
   }
 }
