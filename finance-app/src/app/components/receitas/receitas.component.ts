c7396d1e8063997bdcfd8da82b59ff2e3d2416d9
eduardo <eduardocardoso1039@gmail.com>
2024-05-22 08:47:59 -0300

    Feature | Receitas - Utilizado componente base de transacoes

--- finance-app/src/app/components/receitas/receitas.component.ts
+++ finance-app/src/app/components/receitas/receitas.component.ts
@@ -4,81 +4,18 @@ import { TransacaoUtilService } from '../../utils/transacao-util.service';
 import { ParamsTransacao } from '../../interfaces/ParamsTransacao';
 import { NotificationService } from '../../shared/services/notification.service';
 import { Table } from 'primeng/table';
+import { BaseTransacaoComponent } from '../../shared/components/base-transacao/base-transacao.component';
 
 @Component({
   selector: 'fin-receitas',
   templateUrl: './receitas.component.html',
   styleUrl: './receitas.component.scss',
 })
-export class ReceitasComponent implements OnInit {
-  @ViewChild('dt') dt: Table | undefined;
-
-  protected transacoes: ITransacao[] = [];
-  protected rowSelected!: ITransacao[] | null;
-
-  private queryParams: ParamsTransacao = {
-    filterDate: new Date(),
-    idTipoTransacao: 1
-  };
-
-  constructor(
-    private _notificationService: NotificationService,
-    private _transacaoUtilService: TransacaoUtilService
-  ) {}
-
-  ngOnInit(): void {
-    this.fetchTransacoes(this.queryParams);
-
-    this._notificationService.notifyObservable$.subscribe((res) => {
-      if (res.refresh) {
-        this.fetchTransacoes(this.queryParams);
-      }
-      if (res.closeModal) {
-        this.rowSelected = null;
-      }
-    });
-
-    this._notificationService.notifyObservable$.subscribe((res) => {
-      const { date } = res;
-
-      if (date) {
-        this.queryParams.filterDate = date;
-        this.fetchTransacoes(this.queryParams);
-      }
-    });
-  }
-
-  clear(table: Table) {
-    table.clear();
-  }
-
-  applyFilterGlobal($event: any, stringVal: any) {
-    this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
-  }
-
-  protected deletarTransacoes() {
-    if (this.rowSelected) {
-      const transacoesIds = this.rowSelected.map((item) => item.trs_id);
-      this._transacaoUtilService.deletarTransacoesUtil(transacoesIds);
+export class ReceitasComponent
+  extends BaseTransacaoComponent
+  implements OnInit {
+    override ngOnInit(): void {
+      this.queryParams.idTipoTransacao = 1;
+      super.ngOnInit()
     }
   }
-
-  protected editarTransacao(transacao: ITransacao) {
-    this._transacaoUtilService.editarTransacaoUtil(transacao);
-  }
-
-  protected deletarTransacao(idTransacao: number, isParcelado: boolean) {
-    this._transacaoUtilService.deletarTransacaoUtil(idTransacao, isParcelado);
-  }
-
-  protected checkStatus(transacao: ITransacao): string {
-    return this._transacaoUtilService.checkStatusUtil(transacao);
-  }
-
-  private fetchTransacoes(params: ParamsTransacao) {
-    this._transacaoUtilService.getTransacoesUtil(params).subscribe({
-      next: (transacoes: ITransacao[]) => (this.transacoes = transacoes),
-      error: (err) => console.log(err),
-    });
-  }
-}
