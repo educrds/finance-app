3d9fa9123aac81402ac772d5b0ad74c11898a678
eduardo <eduardocardoso1039@gmail.com>
2024-05-24 21:46:14 -0300

    Refactor | NotificationService - Alterado de BehaviorSubject para Subject por não necessitar de valor inicial.

--- finance-app/src/app/shared/services/notification.service.ts
+++ finance-app/src/app/shared/services/notification.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
-  constructor() {}
+  // Subject que mantém o estado atual da notificação
+  public notify = new Subject<any>();
 
-  public notify = new BehaviorSubject<any>('');
+  // Observable público que permite que outras partes da aplicação se inscrevam para receber notificações
   notifyObservable$ = this.notify.asObservable();
 
+  // Método para emitir novas notificações
   public notifyChanges(data: any) {
     if (data) this.notify.next(data);
   }
