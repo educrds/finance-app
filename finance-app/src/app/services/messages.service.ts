3c804a961f546b8b1ecfe8c6c467716e43c182f0
eduardo <eduardocardoso1039@gmail.com>
2024-05-27 21:29:12 -0300

    Feature | MessagesService - Criado metódo de alerta global.

--- finance-app/src/app/services/messages.service.ts
+++ finance-app/src/app/services/messages.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@angular/core';
-import { MessageService } from 'primeng/api';
+import { Injectable, inject } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessagesService {
-  constructor(private _messageService: MessageService) {}
+  private _confirmationService = inject(ConfirmationService);
+  private _messageService = inject(MessageService);
 
   showSuccess(message: string) {
     this._messageService.add({
@@ -20,6 +21,24 @@ export class MessagesService {
       severity: 'error',
       summary: 'Erro',
       detail: message,
+    });
+  }
+
+  public confirm(
+    message: string,
+    header: string,
+    accept: () => void,
+    reject?: () => void
+  ) {
+    this._confirmationService.confirm({
+      message: message,
+      header: header,
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+      rejectButtonStyleClass: 'p-button-text',
+      accept: accept,
+      reject: reject,
     });
   }
 }
