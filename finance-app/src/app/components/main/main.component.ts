f112ff4f3de758a2fd99ddac81d909d5b7724d13
eduardo <eduardocardoso1039@gmail.com>
2024-05-28 21:00:56 -0300

    Feature | Main - Removido lógicas atreladas ao gráfico de pizza

--- finance-app/src/app/components/main/main.component.ts
+++ finance-app/src/app/components/main/main.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Signal, WritableSignal, computed, signal } from '@angular/core';
 import { Transacao } from '../../interfaces/Transacao';
 import { TransacoesSoma } from '../../interfaces/TransacoesSoma';
-import { ConfigCategoriaChart } from '../../interfaces/Chart';
 import { BaseTransacaoDirective } from '../../shared/directives/base-transacao.directive';
+import Util from '../../shared/utils';
+import { CategoriaChart } from '../../interfaces/Chart';
 
 @Component({
   selector: 'fin-main',
@@ -17,86 +18,20 @@ export class MainComponent extends BaseTransacaoDirective implements OnInit {
   protected saldo: Signal<number> = computed(() => this.somatorio().soma_receitas - this.somatorio().soma_despesas )
 
   // charts
-  protected entradasPorCategoria!: ConfigCategoriaChart;
-  protected saidasPorCategoria!: ConfigCategoriaChart;
-
-  // charts options
-  protected options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    borderWidth: 0,
-    plugins: {
-      legend: {
-        position: 'right',
-        labels: {
-          padding: 15,
-          usePointStyle: true,
-          boxHeight: 10,
-          pointStyle: 'circle',
-        },
-      },
-    },
-  };
+  protected entradasPorCategoria!: CategoriaChart;
+  protected saidasPorCategoria!: CategoriaChart;
 
   // sobreescrevendo metódo da classe extendida
   override afterFetchTransacoes(transacoes: Transacao[]): void {
-    this.somatorio.set(
-      this._transacaoUtilService.obterSomatorioTransacoes(transacoes)
-    );
-    this.calcularSomatorioPorCategoria(transacoes);
+    this.somatorio.set(this._transacaoUtilService.obterSomatorioTransacoes(transacoes));
+
+    const { entrada, saida } = Util.calcularSomatorioPorCategoria(transacoes);
+    this.entradasPorCategoria = entrada;
+    this.saidasPorCategoria = saida;
   }
 
-  // cálculo de transacoes por categoria e tipo para configurar gráfico de pizza
-  private calcularSomatorioPorCategoria(transacoes: Transacao[] | any[]) {
-    const resultado = transacoes.reduce(
-      (acc, transacao) => {
-        const { categoria_cor, categoria_nome, trs_valor, id_tipo_transacao } = transacao;
-        const tipoTransacao = id_tipo_transacao === 1 ? 'entrada' : 'saida';
-
-        const categoriaNome = categoria_nome;
-        const categoriaCor = categoria_cor;
-        const valor = trs_valor;
-
-        // Cria o objeto de categoria se ainda não existir
-        if (!acc[tipoTransacao][categoriaNome]) {
-          acc[tipoTransacao][categoriaNome] = {
-            name: categoriaNome,
-            value: 0,
-            cor: categoriaCor,
-          };
-        }
-
-        // Soma o valor à categoria correspondente
-        acc[tipoTransacao][categoriaNome].value += valor;
-
-        return acc;
-      },
-      { entrada: {}, saida: {} }
-    );
-
-    this.entradasPorCategoria = this.configPieCharts(resultado.entrada);
-    this.saidasPorCategoria = this.configPieCharts(resultado.saida);
-  }
-
-  // configurando gráfico de pizza com dados já modelados
-  private configPieCharts(categoriasPorTipo: ConfigCategoriaChart) {
-    const objCategorias = Object.values(categoriasPorTipo);
-
-    let resultadoPorCategoria = {
-      labels: objCategorias.map((item: any) => item.name),
-      datasets: [
-        {
-          data: objCategorias.map((item: any) => item.value),
-          backgroundColor: objCategorias.map((item: any) => item.cor),
-        },
-      ],
-    };
-
-    return resultadoPorCategoria;
-  }
-
-  // configurando gráfico de pizza com dados já modelados
-  protected sumSelected(transactions: any): number {
+  // Metódo responsável por retornar valor total das transações selecionadas.
+  protected sumSelected(transactions: Transacao[]): number {
     return transactions.reduce(
       (acc: number, transacao: Transacao) => acc + transacao.trs_valor,
       0
