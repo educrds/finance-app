6d3e01a3807f3e9efdeb322e33fbbb69b00e3063
eduardo <eduardocardoso1039@gmail.com>
2024-05-28 21:01:16 -0300

    Feature | Chart - Adicionado tipagens novas do gráfico de pizza.

--- finance-app/src/app/interfaces/Chart.ts
+++ finance-app/src/app/interfaces/Chart.ts
@@ -1,3 +1,18 @@
+export interface CategoriaChartItem {
+  name: string;
+  value: number;
+  cor: string;
+}
+
+export interface CategoriaChart {
+  [key: string]: CategoriaChartItem;
+}
+
+export interface CategoriesGroupedByType {
+  entrada: {[key: string]: CategoriaChartItem},
+  saida: {[key: string]: CategoriaChartItem},
+}
+
 interface Dataset {
   data: number[];
   backgroundColor?: string[];
