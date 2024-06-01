67c79776e097701b71e758fb5c92139663b0c0d0
eduardo <eduardocardoso1039@gmail.com>
2024-06-01 20:50:25 -0300

    Feature | TransacaoUtilService - Adicionado metódo para lidar com exclusão de transações parceladas.

--- finance-app/src/app/shared/services/transacao-util.service.ts
+++ finance-app/src/app/shared/services/transacao-util.service.ts
@@ -8,6 +8,8 @@ import { Observable, catchError, throwError } from 'rxjs';
 import { MessagesService } from '../../services/messages.service';
 import { NotificationService } from './notification.service';
 import { Transacao } from '../../interfaces/Transacao';
+import { ConfirmDialogComponent } from '../../templates/confirm-dialog/confirm-dialog.component';
+import { ConfirmDialogService } from './confirm-dialog.service';
 
 @Injectable({
   providedIn: 'root',
@@ -19,7 +21,8 @@ export class TransacaoUtilService {
     private _transacoesService: TransacoesService,
     private _notificationService: NotificationService,
     private _messagesService: MessagesService,
-    private _dialogService: DialogService
+    private _dialogService: DialogService,
+    private _confirmDialogService: ConfirmDialogService,
   ) {}
 
   obterSomatorioTransacoes(transacoes: Transacao[]): TransacoesSoma {
@@ -37,32 +40,85 @@ export class TransacaoUtilService {
     );
   }
 
-  deletarTransacaoUtil(idTransacao: number, isParcelado: boolean) {
-    let confirmationMessage: string;
-    let successMessage: string;
-    let errorMessage: string;
+  deletarTransacaoUtil(transacao: Transacao) {
+    let confirmationMessage =
+      'Deseja realmente excluir o registro? Esta ação é irreversível.';
+    let successMessage = 'Registro deletado com sucesso!';
+    let errorMessage = 'Ocorreu um erro ao deletar registro!';
 
-    if (isParcelado) {
-      confirmationMessage = `Este registro trata-se de uma transação que se repete. <br>
-        Esta ação deletará também as transações relativas as mese(s) posterior(es). 
-        Deseja prosseguir?`;
-      successMessage = 'Registros deletados com sucesso!';
-      errorMessage = 'Ocorreu um erro ao deletar registros!';
+    if (transacao.trs_parcelado) {
+      // lida com a lógica de excluir TODOS ou apenas UM
+      this.deletarTransacoesParceladas(transacao);
     } else {
-      confirmationMessage =
-        'Deseja realmente excluir o registro? Esta ação é irreversível.';
-      successMessage = 'Registro deletado com sucesso!';
-      errorMessage = 'Ocorreu um erro ao deletar registro!';
-    }
-
-    this._messagesService.confirm(confirmationMessage, 'Confirmação', () => {
-      this._transacoesService.deletarTransacao(idTransacao).subscribe({
-        next: () => {
-          this._messagesService.showSuccess(successMessage);
-          this._notificationService.notifyChanges({ refresh: true });
-        },
-        error: () => this._messagesService.showError(errorMessage),
+      this._messagesService.confirm(confirmationMessage, 'Confirmação', () => {
+        // transformar em um metódo pq vai ser chamado em mais de um local
+        this._transacoesService.deletarTransacao(transacao.trs_id).subscribe({
+          next: () => {
+            this._messagesService.showSuccess(successMessage);
+            this._notificationService.notifyChanges({ refresh: true });
+          },
+          error: () => this._messagesService.showError(errorMessage),
+        });
       });
+    }
+  }
+
+  deletarTransacoesParceladas(transacao: Transacao) {
+    const { par_id, trs_id } = transacao;
+
+    const configModal = {
+      modal: true,
+      header: 'Confirmar remoção',
+      width: '35vw',
+      height: '15vw',
+      contentStyle: { overflow: 'auto' },
+    };
+
+    const confirmationMessage = `Este registro trata-se de uma transação que se repete.<br>
+    Deseja deletar <b>todos os registros</b> ou <b>apenas esse</b>?`;
+    let successMessage = 'Registro deletado com sucesso!';
+    let errorMessage = 'Ocorreu um erro ao deletar registro!';
+
+    this._ref = this._dialogService.open(ConfirmDialogComponent, {
+      ...configModal,
+      data: {
+        acceptLabel: 'Apenas selecionado',
+        rejectLabel: 'Todas',
+        body: confirmationMessage
+      },
+    });
+
+    this._confirmDialogService.config$.subscribe((res) => {
+      if (res.accept) {
+        if (par_id !== null) {
+          return this._transacoesService
+            .deletarParcelaTransacao(par_id)
+            .subscribe({
+              next: () => {
+                this._messagesService.showSuccess(successMessage);
+                this._notificationService.notifyChanges({ refresh: true }, this._ref);
+              },
+              error: () => this._messagesService.showError(errorMessage),
+            });
+        }
+        return this._transacoesService.deletarTransacao(trs_id).subscribe({
+          next: () => {
+            this._messagesService.showSuccess(successMessage);
+            this._notificationService.notifyChanges({ refresh: true }, this._ref);
+          },
+          error: () => this._messagesService.showError(errorMessage),
+        });
+      } else {
+        return this._transacoesService
+          .deletarTodasTransacoesById(trs_id)
+          .subscribe({
+            next: () => {
+              this._messagesService.showSuccess(successMessage);
+              this._notificationService.notifyChanges({ refresh: true }, this._ref);
+            },
+            error: () => this._messagesService.showError(errorMessage),
+          });
+      }
     });
   }
 
@@ -73,8 +129,8 @@ export class TransacaoUtilService {
     let errorMessage: string = 'Ocorreu um erro ao deletar registros!';
 
     this._messagesService.confirm(confirmationMessage, 'Confirmação', () => {
-      transacoesIds.map((idTransacao) => {
-        this._transacoesService.deletarTransacao(idTransacao).subscribe({
+      transacoesIds.map((id_transacao) => {
+        this._transacoesService.deletarTransacao(id_transacao).subscribe({
           next: () => {
             this._messagesService.showSuccess(successMessage);
             this._notificationService.notifyChanges({ refresh: true });
