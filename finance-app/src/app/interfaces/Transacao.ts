326bed72886847ac6f0d189b70f467f9d16df989
eduardo <eduardocardoso1039@gmail.com>
2024-06-01 20:54:22 -0300

    Refactor | Transacao - Adicionado nova tipagem de transacao

--- finance-app/src/app/interfaces/Transacao.ts
+++ finance-app/src/app/interfaces/Transacao.ts
@@ -1,16 +1,18 @@
 export type Transacao = {
-  trs_data_ocorrido: Date;
+  trs_data_ocorrido: string;
+  trs_ano_ocorrido: number;
+  trs_mes_ocorrido: number;
+  trs_id: number;
   trs_valor: number;
   trs_titulo: string;
-  trs_tipo: number;
-  trs_id: number;
   trs_status: number;
   categoria_nome: string;
-  id_categoria: number;
   categoria_cor: string;
+  id_categoria: number;
   metodo_nome: string;
   metodo_id: number;
   tipo_transacao: string;
   id_tipo_transacao: number;
   trs_parcelado: number;
+  par_id: number | null;
 };
