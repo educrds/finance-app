25043b8d03066f492e3b075b7594afb6229abe5a
eduardo <eduardocardoso1039@gmail.com>
2024-06-01 20:46:36 -0300

    Feature | ConfirmDialog - Criado modal para lidar com exclusão de transações parceladas.

---
+++ finance-app/src/app/templates/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { ConfirmDialogService } from '../../shared/services/confirm-dialog.service';
+
+@Component({
+  selector: 'fin-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss'
+})
+export class ConfirmDialogComponent implements OnInit {
+  protected config: any = {
+    acceptLabel: 'Confirmar',
+    rejectLabel: 'Cancelar',
+    body: null
+  };
+
+  private _config = inject(DynamicDialogConfig);
+  private _confirmDialogService = inject(ConfirmDialogService);
+
+  ngOnInit(): void {
+    if(this._config.data){  
+      this.config = this._config.data;
+    }
+  }
+
+  onAccept(){
+    this._confirmDialogService.accept();
+  }
+  
+  onReject(){
+    this._confirmDialogService.reject();
+  }
+}
