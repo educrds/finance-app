c242ec513a60a7b4c16022c817b435332f2f9b0d
eduardo <eduardocardoso1039@gmail.com>
2024-06-01 21:00:37 -0300

    REST API | Delete - Realizando soft delete ao invés de delete

--- node-server/queries/transacoes/DELETE/index.js
+++ node-server/queries/transacoes/DELETE/index.js
@@ -1,1 +1,2 @@
-export const delete_transacao_by_id = `DELETE FROM tb_transacoes WHERE  trs_id=?`
+export const delete_transacao_by_id = `UPDATE tb_transacoes SET trs_deleted=1 WHERE trs_id=?`
+export const delete_transacao_parcela_by_id = `UPDATE tb_parcelas SET par_deleted=1 WHERE par_id=?`
