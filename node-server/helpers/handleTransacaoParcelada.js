e09cb782e3fd50ea1f3739b22f9e5b9e871f4309
eduardo <eduardocardoso1039@gmail.com>
2024-06-01 21:42:35 -0300

    REST API | HandleTransacaoParcelada - Refatorado metódo que lida com a lógica de parceladas

--- node-server/helpers/handleTransacaoParcelada.js
+++ node-server/helpers/handleTransacaoParcelada.js
@@ -1,8 +1,10 @@
-import { executeBatch } from '../config/db.config.js';
+import { executeBatch, executeQuery } from '../config/db.config.js';
 import { calcularRepeticoes } from './calcularRepeticoes.js';
 
 export async function handleTransacaoParcelada(insertId, data_ocorrido, data_fim_repeticao) {
-  const transacoes_parcelas = calcularRepeticoes(insertId, data_ocorrido, data_fim_repeticao);
+  const result_rel_parcelas = await executeQuery('INSERT INTO tb_rel_parcelas(trs_pai_id) VALUES(?)', insertId);
+  const id_rel_parcelas = result_rel_parcelas?.insertId;
+  const transacoes_parcelas = calcularRepeticoes(id_rel_parcelas, data_ocorrido, data_fim_repeticao);
   const resultBatch = await executeBatch('INSERT INTO tb_parcelas(trs_pai_id, ano, mes) VALUES(?, ?, ?)', transacoes_parcelas);
 
   if (resultBatch.affectedRows <= 0) {
