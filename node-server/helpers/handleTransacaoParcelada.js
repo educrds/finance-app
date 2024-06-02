2bb16c8a2998a53574d2981144e67409a47a97a9
eduardo <eduardocardoso1039@gmail.com>
2024-06-02 12:20:39 -0300

    REST API | HandleTransacaoParcelada - Nova lógica baseada na nova modelagem de dados

--- node-server/helpers/handleTransacaoParcelada.js
+++ node-server/helpers/handleTransacaoParcelada.js
@@ -1,11 +1,9 @@
-import { executeBatch, executeQuery } from '../config/db.config.js';
+import { executeBatch } from '../config/db.config.js';
 import { calcularRepeticoes } from './calcularRepeticoes.js';
 
-export async function handleTransacaoParcelada(insertId, data_ocorrido, data_fim_repeticao) {
-  const result_rel_parcelas = await executeQuery('INSERT INTO tb_rel_parcelas(trs_pai_id) VALUES(?)', insertId);
-  const id_rel_parcelas = result_rel_parcelas?.insertId;
-  const transacoes_parcelas = calcularRepeticoes(id_rel_parcelas, data_ocorrido, data_fim_repeticao);
-  const resultBatch = await executeBatch('INSERT INTO tb_parcelas(trs_pai_id, ano, mes) VALUES(?, ?, ?)', transacoes_parcelas);
+export async function handleTransacaoParcelada(insertId, data_ocorrido, data_fim_repeticao, user_id) {
+  const transacoes_parcelas = calcularRepeticoes(insertId, data_ocorrido, data_fim_repeticao, user_id);
+  const resultBatch = await executeBatch('INSERT INTO tb_parcelas(trs_pai_id, ano, mes, usr_id) VALUES(?, ?, ?, ?)', transacoes_parcelas);
 
   if (resultBatch.affectedRows <= 0) {
     throw new Error('Ocorreu um erro ao adicionar os registros de parcelas.');
