438eee7587476846c98f4f0a1618def3b8fc9474
eduardo <eduardocardoso1039@gmail.com>
2024-06-11 08:16:08 -0300

    Feature | BaseTransacao - Adicionado operador takeUntil e destroy do subject de observables

--- finance-app/src/app/shared/directives/base-transacao.directive.ts
+++ finance-app/src/app/shared/directives/base-transacao.directive.ts
@@ -3,60 +3,68 @@ import { Transacao } from '../../interfaces/Transacao';
 import { ParamsTransacao } from '../../interfaces/ParamsTransacao';
 import { TransacaoUtilService } from '../services/transacao-util.service';
 import { NotificationService } from '../services/notification.service';
-import { Subscription } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { DatePickerService } from '../../services/date-picker.service';
 import { TransacoesService } from '../../services/transacoes.service';
 
 @Directive({
-  selector: '[finBaseTransacao]'
+  selector: '[finBaseTransacao]',
 })
 export class BaseTransacaoDirective implements OnInit, OnDestroy {
   protected _notificationService = inject(NotificationService);
   protected _transacaoUtilService = inject(TransacaoUtilService);
   protected _transacoesService = inject(TransacoesService);
   protected _datePickerService = inject(DatePickerService);
-  
+
   protected transacoes: WritableSignal<Transacao[]> = signal([]);
   protected rowSelected: WritableSignal<Transacao[]> = signal([]);
   protected queryParams: WritableSignal<ParamsTransacao> = signal({});
 
-  private _subscription!: Subscription;
+  private _destroy$ = new Subject<void>();
 
   ngOnInit(): void {
-    this._subscription = this._datePickerService.datePickerObservable$.subscribe((date) => {
-      if (date) {
-        this.queryParams().filterDate = date;
-        this.fetchTransacoes(this.queryParams());
-      }
-    })
+    this._datePickerService.datePickerObservable$
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((date) => {
+        if (date) {
+          this.queryParams().filterDate = date;
+          this.fetchTransacoes(this.queryParams());
+        }
+      });
 
-    this._subscription = this._notificationService.notifyObservable$.subscribe((res) => {
-      const { refresh, closeModal } = res;
+    this._notificationService.notifyObservable$
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((res) => {
+        const { refresh, closeModal } = res;
 
-      if (refresh) {
-        this.fetchTransacoes(this.queryParams());
-      }
+        if (refresh) {
+          this.fetchTransacoes(this.queryParams());
+        }
 
-      if (closeModal) {
-        this.rowSelected.set([]);
-      }
-    });
-    
+        if (closeModal) {
+          this.rowSelected.set([]);
+        }
+      });
   }
 
   private fetchTransacoes(params: ParamsTransacao) {
-    this._subscription = this._transacoesService.getTransacoes(params).subscribe({
-      next: (transacoes: Transacao[]) => {
-        this.transacoes.set(transacoes);
-        this.afterFetchTransacoes(transacoes);
-      },
-      error: (err) => console.log(err),
-    });
+    this._transacoesService
+      .getTransacoes(params)
+      .pipe(takeUntil(this._destroy$))
+      .subscribe({
+        next: (transacoes: Transacao[]) => {
+          this.transacoes.set(transacoes);
+          this.afterFetchTransacoes(transacoes);
+        },
+        error: (err) => console.log(err),
+      });
   }
 
   protected afterFetchTransacoes(transacoes: Transacao[]): void {}
 
   ngOnDestroy(): void {
-    if(this._subscription) this._subscription.unsubscribe();
+    this._destroy$.next();  // Emite um valor para todos os observadores
+    this._destroy$.complete();  // Completa o Subject
   }
+  
 }
