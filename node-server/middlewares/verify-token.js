42e0d01ff5f7525ff4770294943abf0fb9333268
eduardo <eduardocardoso1039@gmail.com>
2024-06-15 16:51:55 -0300

    REST API | Middleware - Refatorado lógica de middleware para validar corretamente os tokens

--- node-server/middlewares/verify-token.js
+++ node-server/middlewares/verify-token.js
@@ -1,19 +1,67 @@
 import jwt from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
+import dotenv from 'dotenv';
 
-export const verifyToken = (req, res, next) => {
+const process = dotenv.config();
+
+const client = jwksClient({
+  jwksUri: process.parsed.AUTH0_KEYS,
+});
+
+// Caminho para a chave privada local
+export const privateKey = process.parsed.PRIVATE_KEY;
+
+// Função para obter a chave pública do JWKS
+const getKey = (header) => {
+  return new Promise((resolve, reject) => {
+    client.getSigningKey(header.kid, (err, key) => {
+      if (err) {
+        reject(err);
+      } else {
+        const signingKey = key.getPublicKey();
+        resolve(signingKey);
+      }
+    });
+  });
+};
+
+// Função para verificar o token JWT
+const verifyJwtToken = (token, key, algorithms) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, key, { algorithms }, (err, decoded) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(decoded);
+      }
+    });
+  });
+};
+
+// Middleware para verificar token JWT
+export const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-    res.status(401).send({ message: 'Usuário não autenticado.' });
-    return;
+    return res.status(401).send({ message: 'Usuário não autenticado.' });
   }
 
-  jwt.verify(token, 'seu_segredo_secreto', (err, decoded) => {
-    if (err) {
-      res.status(403).send({ message: 'Token inválido.' });
-      return;
+  try {
+    const decodedToken = jwt.decode(token, { complete: true });
+    let decoded;
+
+    if (decodedToken && decodedToken.header?.kid) {
+      // Token emitido pelo Auth0
+      const key = await getKey(decodedToken.header);
+      decoded = await verifyJwtToken(token, key, ['RS256']);
+    } else {
+      // Token gerado localmente com jwt.sign
+      decoded = await verifyJwtToken(token, privateKey, ['RS256']);
     }
+
     req.body.user = decoded;
     next();
-  });
+  } catch (err) {
+    res.status(403).send({ message: 'Token inválido.' });
+  }
 };
