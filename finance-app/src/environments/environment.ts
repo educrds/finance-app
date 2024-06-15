1e3f8fd404386e0ed56009ae99065a366d8b3006
eduardo <eduardocardoso1039@gmail.com>
2024-06-15 16:50:28 -0300

    Feature | Environment - Adicionado configuracoes do auth0

--- finance-app/src/environments/environment.ts
+++ finance-app/src/environments/environment.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  api_url: "http://192.168.1.20:4201/"
+  api_url: "http://192.168.1.20:4201/",
+  auth0: {
+    domain: 'dev-mcn1uekwdmke2f2j.us.auth0.com',
+    clientId: 'XU4IMObXqidJ2St3FFXLFPlOpJhqrL9I',
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    },
+  }
 };
