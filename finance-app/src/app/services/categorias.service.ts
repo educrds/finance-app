2160d9bd63e190f2359695ab3c240047c118103d
eduardo <eduardocardoso1039@gmail.com>
2024-06-15 18:16:22 -0300

    Refactor | CategoriasService - Adicionado operador shareReplay para compartilhar observables sem chamar novamente o endpoint

--- finance-app/src/app/services/categorias.service.ts
+++ finance-app/src/app/services/categorias.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, retry } from 'rxjs';
+import { Observable, retry, shareReplay } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { IDropdown } from '../interfaces/Dropdown';
 import { Categoria, Categorias } from '../interfaces/Categorias';
@@ -10,39 +10,39 @@ import { ICategoriasService } from '../interfaces/ICategoriasService';
   providedIn: 'root',
 })
 export class CategoriasService implements ICategoriasService {
-  private _api_url = environment.api_url;
+  #_api_url = environment.api_url;
 
   constructor(private _http: HttpClient) {}
 
   getCategoriasDropdown(cat_tip_id: number): Observable<IDropdown[]> {
     return this._http
-      .post<IDropdown[]>(`${this._api_url}categorias/listar-select`, {
+      .post<IDropdown[]>(`${this.#_api_url}categorias/listar-select`, {
         data: { cat_tip_id: cat_tip_id },
       })
-      .pipe(retry(1));
+      .pipe(retry(1), shareReplay(1));
   }
 
   getCategorias(): Observable<Categorias[]> {
     return this._http
-      .post<Categorias[]>(`${this._api_url}categorias/listar`, {})
-      .pipe(retry(1));
+      .post<Categorias[]>(`${this.#_api_url}categorias/listar`, {})
+      .pipe(retry(1), shareReplay(1));
   }
 
   addCategoria(form: Categoria): Observable<Categoria> {
     return this._http
-      .post<Categoria>(`${this._api_url}categoria/adicionar`, { data: form })
+      .post<Categoria>(`${this.#_api_url}categoria/adicionar`, { data: form })
       .pipe(retry(1));
   }
 
   atualizarCategoria(form: Categoria): Observable<Categoria> {
     return this._http
-      .post<Categoria>(`${this._api_url}categoria/atualizar`, { data: form })
+      .post<Categoria>(`${this.#_api_url}categoria/atualizar`, { data: form })
       .pipe(retry(1));
   }
 
   deletarCategoria(form: Categoria): Observable<Categoria> {
     return this._http
-      .post<Categoria>(`${this._api_url}categoria/deletar`, { data: form })
+      .post<Categoria>(`${this.#_api_url}categoria/deletar`, { data: form })
       .pipe(retry(1));
   }
 }
