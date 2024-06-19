8cf4400c27c910ad554748e6ee141706211fed75
eduardo <eduardocardoso1039@gmail.com>
2024-06-19 20:09:58 -0300

    Refactor | HttpInterceptor - Adicionado modificador de acesso privado e inject

--- finance-app/src/app/helpers/http-interceptor.interceptor.ts
+++ finance-app/src/app/helpers/http-interceptor.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { StorageService } from '../shared/services/storage.service';
@@ -13,25 +13,23 @@ import { HttpErrorHandlerService } from '../shared/services/http-error-handler.s
 
 @Injectable()
 export class ApiRequestInterceptor implements HttpInterceptor {
-  constructor(
-    private _storageService: StorageService,
-    private _router: Router,
-    private _httpErrorHandlerService: HttpErrorHandlerService
-  ) {}
+    #_storageService = inject(StorageService);
+    #_router = inject(Router);
+    #_httpErrorHandlerService = inject(HttpErrorHandlerService);
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const authRoutes = req.url.includes('/login') || req.url.includes('/register');
 
     if (authRoutes) {
       return next.handle(req).pipe(
-        catchError((err: HttpErrorResponse) => this._httpErrorHandlerService.handleHttpError(err))
+        catchError((err: HttpErrorResponse) => this.#_httpErrorHandlerService.handleHttpError(err))
       );
     }
 
     // Verifica usuario logado e segue com a requisiçao enviando payload modificado
     // com headers e id do usuário no body.
-    if (this._storageService.isLoggedIn()) {
-      const token = this._storageService.getToken;
+    if (this.#_storageService.isLoggedIn()) {
+      const token = this.#_storageService.getToken;
 
       req = req.clone({
         setHeaders: {
@@ -41,10 +39,10 @@ export class ApiRequestInterceptor implements HttpInterceptor {
 
       return next
         .handle(req)
-        .pipe(catchError((error: HttpErrorResponse) => this._httpErrorHandlerService.handleHttpError(error)));
+        .pipe(catchError((error: HttpErrorResponse) => this.#_httpErrorHandlerService.handleHttpError(error)));
     }
 
-    this._router.navigate(['/auth/login']);
+    this.#_router.navigate(['/auth/login']);
     return throwError('Usuário não autenticado');
   }
 }
