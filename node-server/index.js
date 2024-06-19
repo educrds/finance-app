213fef85b5c173e2310f52b8d3dd6ea0c83d7cb0
eduardo <eduardocardoso1039@gmail.com>
2024-06-19 20:05:24 -0300

    REST API | Server - Adicionado controllers para lidar com rotas

--- node-server/index.js
+++ node-server/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-import transacoes from './routes/transacoes/todas-transacoes.js';
-import adicionar_transacao from './routes/transacoes/adicionar-transacao.js';
+// routes
+import categorias from './routes/categorias/index.js';
+import transacoes from './routes/transacoes/index.js';
+import charts from './routes/charts/index.js';
+import user from './routes/user/index.js';
 
-import categorias from './routes/categorias/todas-categorias.js';
-
-import authUser from './routes/user/auth-user.js';
 import { verifyToken } from './middlewares/verify-token.js';
 
 const app = express();
@@ -16,56 +16,9 @@ const PORT = 4201;
 
 app.use(bodyParser.json(), cors(), bodyParser.urlencoded({ extended: true }));
 
-// -------------------- TRANSAÇÕES
+app.use('/categoria', verifyToken, categorias);
+app.use('/transacao', verifyToken, transacoes);
+app.use('/chart', verifyToken, charts);
+app.use('/user', user);
 
-// LISTAR TRANSAÇÕES
-app.post('/transacoes/listar', verifyToken, transacoes);
-
-// DELETAR TRANSAÇÕES
-app.post('/transacao/deletar', verifyToken, transacoes);
-
-// DELETAR TODAS TRANSAÇÕES POR ID
-app.post('/transacao/deletar-todas', verifyToken, transacoes);
-
-// ATUALIZAR TRANSAÇÕES
-app.post('/transacao/atualizar', verifyToken, transacoes);
-
-// ADICIONAR TRANSAÇÕES
-app.post('/transacao/adicionar', verifyToken, adicionar_transacao);
-
-// LISTAR METÓDOS TRANSAÇÕES
-app.post('/transacoes/listar/metodos', verifyToken, transacoes);
-
-
-// -------------------- CHARTS
-app.post('/charts/comparativo-anual', verifyToken, transacoes);
-
-
-// -------------------- CATEGORIAS
-
-// ADD TRANSAÇÕES
-app.post('/categorias/listar', verifyToken, categorias);
-
-// ADD TRANSAÇÕES
-app.post('/categorias/listar-select', verifyToken, categorias);
-
-// ADD TRANSAÇÕES
-app.post('/categoria/adicionar', verifyToken, categorias);
-
-// ADD TRANSAÇÕES
-app.post('/categoria/atualizar', verifyToken, categorias);
-
-// ADD TRANSAÇÕES
-app.post('/categoria/deletar', verifyToken, categorias);
-
-
-
-// -------------------- USUARIO
-
-// LOGIN USUÁRIO
-app.post('/user/login', authUser);
-
-// REGISTRA USUÁRIO
-app.post('/user/register', authUser);
-
-app.listen(PORT, '192.168.1.20', () => console.log(`server is running on port: http://localhost:`));
+app.listen(PORT, '192.168.1.20', () => console.log(`server is running on port: http://localhost: ${PORT}`));
