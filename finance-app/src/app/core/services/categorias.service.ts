3960b486ec3f4f22ef206e2bf8e559d0854c4236
eduardo <eduardocardoso1039@gmail.com>
2024-06-23 19:30:45 -0300

    Docs | CategoriasService - Documentando metódos da service de categorias.

--- finance-app/src/app/core/services/categorias.service.ts
+++ finance-app/src/app/core/services/categorias.service.ts
@@ -13,32 +13,56 @@ export class CategoriasService implements ICategoriasService {
   #_api_url = environment.api_url;
   #_http = inject(HttpClient)
 
+  /**
+   * Obtém uma lista de categorias formatadas para um dropdown baseado no ID do tipo de categoria.
+   * @param cat_tip_id - O ID do tipo de categoria para filtrar as categorias.
+   * @returns Um Observable contendo uma lista de objetos IDropdown.
+   */
   getCategoriasDropdown$(cat_tip_id: number): Observable<IDropdown[]> {
     return this.#_http
       .post<IDropdown[]>(`${this.#_api_url}categoria/listar-select-categorias`, {
-        data: { cat_tip_id: cat_tip_id },
+        data: { cat_tip_id },
       })
       .pipe(retry(1), shareReplay(1));
   }
 
+  /**
+   * Obtém uma lista de categorias associadas ao usuário atual.
+   * @returns Um Observable contendo uma lista de objetos Categorias.
+   */
   getCategoriasByUser$(): Observable<Categorias[]> {
     return this.#_http
       .post<Categorias[]>(`${this.#_api_url}categoria/listar-categorias`, {})
       .pipe(retry(1), shareReplay(1));
   }
 
+  /**
+   * Adiciona uma nova categoria.
+   * @param form - Um objeto do tipo Categoria contendo os dados da categoria a ser adicionada.
+   * @returns Um Observable contendo o objeto Categoria adicionado.
+   */
   addCategoria$(form: Categoria): Observable<Categoria> {
     return this.#_http
       .post<Categoria>(`${this.#_api_url}categoria/adicionar-categoria`, { data: form })
       .pipe(retry(1));
   }
 
+  /**
+   * Atualiza uma categoria existente.
+   * @param form - Um objeto do tipo Categoria contendo os dados da categoria a ser atualizada.
+   * @returns Um Observable contendo o objeto Categoria atualizado.
+   */
   atualizarCategoria$(form: Categoria): Observable<Categoria> {
     return this.#_http
       .post<Categoria>(`${this.#_api_url}categoria/atualizar-categoria`, { data: form })
       .pipe(retry(1));
   }
 
+  /**
+   * Deleta uma categoria.
+   * @param form - Um objeto do tipo Categoria contendo os dados da categoria a ser deletada.
+   * @returns Um Observable contendo o objeto Categoria deletado.
+   */
   deletarCategoria$(form: Categoria): Observable<Categoria> {
     return this.#_http
       .post<Categoria>(`${this.#_api_url}categoria/deletar-categoria`, { data: form })
