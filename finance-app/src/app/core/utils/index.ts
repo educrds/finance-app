6b118588ac8e3a485570d80a303776a8c2aa2109
eduardo <eduardocardoso1039@gmail.com>
2024-07-30 19:10:17 -0300

    Feature | CoreUtil - Adicionado método para ordenar transação por método

--- finance-app/src/app/core/utils/index.ts
+++ finance-app/src/app/core/utils/index.ts
@@ -17,6 +17,31 @@ export default class CoreUtil {
     }
     return "X";
   }
+
+  static orderByMetodo(transacoes: Transacao[] | any[]): CategoriesGroupedByType {
+    return transacoes.reduce((acc: CategoriesGroupedByType, transacao: Transacao) => {
+      const { categoria_cor, metodo_nome, trs_valor, id_tipo_transacao } = transacao;
+  
+      // Filtra transações pelo tipo
+      if (id_tipo_transacao !== 1) {
+        // Verifica se o método já existe no acumulador
+        if (!acc[metodo_nome]) {
+          // Cria um novo objeto para o método
+          acc[metodo_nome] = {
+            name: metodo_nome,
+            value: 0,
+            cor: categoria_cor,
+          };
+        }
+        
+        // Soma o valor ao método existente
+        acc[metodo_nome].value += trs_valor;
+      }
+  
+      return acc;
+    }, {});
+  }
+  
   
   // cálculo de transacoes por categoria e tipo para configurar gráfico de pizza
   static calcularSomatorioPorCategoria(transacoes: Transacao[] | any[]): CategoriesGroupedByType {
