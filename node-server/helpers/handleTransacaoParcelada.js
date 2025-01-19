d97d88820e653edab17270e75477e7b1c38b5d58
eduardo <eduardocardoso1039@gmail.com>
2025-01-19 10:22:48 -0300

    REST API | HandleTransacaoParcelada - Adicionado nova propriedade para parcelas

--- node-server/helpers/handleTransacaoParcelada.js
+++ node-server/helpers/handleTransacaoParcelada.js
@@ -3,7 +3,7 @@ import { calcularRepeticoes } from './calcularRepeticoes.js';
 
 export async function handleTransacaoParcelada(insertId, data_ocorrido, data_fim_repeticao, user_id) {
   const transacoes_parcelas = calcularRepeticoes(insertId, data_ocorrido, data_fim_repeticao, user_id);
-  const resultBatch = await executeBatch('INSERT INTO tb_parcelas(trs_pai_id, ano, mes, usr_id) VALUES(?, ?, ?, ?)', transacoes_parcelas);
+  const resultBatch = await executeBatch('INSERT INTO tb_parcelas(trs_pai_id, ano, mes, usr_id, num_parcela) VALUES(?, ?, ?, ?, ?)', transacoes_parcelas);
 
   if (resultBatch.affectedRows <= 0) {
     throw new Error('Ocorreu um erro ao adicionar os registros de parcelas.');
