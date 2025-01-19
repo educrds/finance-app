eaa6eabcf221182d384b544a8d1e339d2b61cd48
eduardo <eduardocardoso1039@gmail.com>
2025-01-19 10:23:37 -0300

    REST API | InsertTransacoes - Atualizado query de inserção de transacao

--- node-server/queries/transacoes/INSERT/index.js
+++ node-server/queries/transacoes/INSERT/index.js
@@ -1,3 +1,2 @@
 export const insert_transacao = `
-INSERT INTO tb_transacoes (trs_valor, trs_data_ocorrido, trs_titulo, trs_categoria, trs_usuario, trs_tipo, trs_metodo, trs_parcelado, trs_status
-) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
+INSERT INTO tb_transacoes (trs_valor, trs_data_ocorrido, trs_titulo, trs_categoria, trs_usuario, trs_tipo, trs_metodo, trs_parcelado, trs_status, trs_num_parcela) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
