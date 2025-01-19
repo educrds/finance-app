36e5da04d09d19b4118e9b0fb6d3a9afc63714e3
eduardo <eduardocardoso1039@gmail.com>
2025-01-19 20:13:47 -0300

    Refactor | TransacaoTable - Adicionado novos filtros de tabela

--- finance-app/src/app/core/components/transacao-table/transacao-table.component.ts
+++ finance-app/src/app/core/components/transacao-table/transacao-table.component.ts
@@ -1,11 +1,8 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild, WritableSignal, inject } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild, WritableSignal, inject, signal } from "@angular/core";
 import { Transacao } from "../../models/Transacao";
 import { Table, TableModule } from "primeng/table";
 import { TransacaoUtilService } from "../../services/transacao-util.service";
 import SharedUtil from "../../../shared/utils";
-import { CategoriasService } from "../../services/categorias.service";
-import { Observable } from "rxjs";
-import { IDropdown } from "../../models/Dropdown";
 import { SelectionIntervalRowsDirective } from "../../directives/selection-interval-rows.directive";
 import { PrimeTemplate } from "primeng/api";
 import { InputTextModule } from "primeng/inputtext";
@@ -13,29 +10,55 @@ import { ButtonDirective } from "primeng/button";
 import { NgClass, NgStyle, CurrencyPipe, DatePipe } from "@angular/common";
 import { AlertContainerComponent } from "../alert-container/alert-container.component";
 import { SplitButtonModule } from "primeng/splitbutton";
+import { IconField } from "primeng/iconfield";
+import { InputIcon } from "primeng/inputicon";
+import { Select } from 'primeng/select';
+import { FormsModule } from "@angular/forms";
 
 @Component({
-    selector: "fin-transacao-table",
-    templateUrl: "./transacao-table.component.html",
-    styleUrl: "./transacao-table.component.scss",
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: true,
-    imports: [TableModule, SelectionIntervalRowsDirective, PrimeTemplate, InputTextModule, ButtonDirective, NgClass, NgStyle, AlertContainerComponent, CurrencyPipe, DatePipe, SplitButtonModule]
+  selector: "fin-transacao-table",
+  templateUrl: "./transacao-table.component.html",
+  styleUrl: "./transacao-table.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    IconField,
+    InputIcon,
+    TableModule,
+    SelectionIntervalRowsDirective,
+    PrimeTemplate,
+    InputTextModule,
+    ButtonDirective,
+    NgClass,
+    NgStyle,
+    Select,
+    AlertContainerComponent,
+    CurrencyPipe,
+    DatePipe,
+    SplitButtonModule,
+    FormsModule
+  ],
 })
-export class TransacaoTableComponent implements OnInit {
+export class TransacaoTableComponent implements OnChanges {
   @ViewChild("dt") dt: Table | undefined;
   @ViewChild("inputSearch") inputSearch: ElementRef | undefined;
 
   @Input() transacoes: Transacao[] = [];
   @Input() rowSelected: Transacao[] = [];
 
-  protected categoriasOptions$!: Observable<IDropdown[]>;
+  protected categoriasOptions: WritableSignal<string[]> = signal([]);
+  protected selectedCategory: string | null = null;
+
+  protected metodosOptions: WritableSignal<string[]> = signal([]);
+  protected selectedMetodo: string | null = null;
 
   private _transacaoUtilService = inject(TransacaoUtilService);
-  private _categoriasService = inject(CategoriasService);
 
-  ngOnInit(): void {
-    // this.categoriasOptions$ = this._categoriasService.getCategoriasDropdown$();
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes["transacoes"] && changes["transacoes"].currentValue.length){
+      this.categoriasOptions.set([...new Set(this.transacoes.map((i) => i.categoria_nome))]);
+      this.metodosOptions.set([...new Set(this.transacoes.map((i) => i.metodo_nome))]);
+    }
   }
 
   protected clear(table: Table): void {
@@ -45,8 +68,11 @@ export class TransacaoTableComponent implements OnInit {
     table.clear();
   }
 
-  protected applyFilterGlobal($event: any, stringVal: any): void {
-    this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  protected applyFilterGlobal($event: any, stringVal: string = "contains"): void {
+    const value = $event.target instanceof HTMLInputElement 
+      ? ($event.target as HTMLInputElement).value 
+      : $event;
+    this.dt!.filterGlobal(value, stringVal);
   }
 
   protected updateSelectionRows(newSelection: Transacao[]): void {
@@ -54,7 +80,7 @@ export class TransacaoTableComponent implements OnInit {
   }
 
   // configurando gráfico de pizza com dados já modelados
-  protected sumSelected(transactions: any): number {
+  protected sumSelected(transactions: Transacao[]): number {
     return transactions.reduce((acc: number, transacao: Transacao) => acc + transacao.trs_valor, 0);
   }
 
@@ -68,7 +94,7 @@ export class TransacaoTableComponent implements OnInit {
 
   protected deletarTransacoes(): void {
     if (this.rowSelected) {
-      const transacoesIds = this.rowSelected.map((item) => item.trs_id);
+      const transacoesIds = this.rowSelected.map(item => item.trs_id);
       this._transacaoUtilService.deletarTransacoesUtil(transacoesIds);
     }
   }
