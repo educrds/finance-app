1aa308691a3ca6d87c89b5987f01c3cf2cbb280b
eduardo <eduardocardoso1039@gmail.com>
2025-01-19 10:16:55 -0300

    Refactor | AlertContainer - Atualizado input para signals

--- finance-app/src/app/core/components/alert-container/alert-container.component.ts
+++ finance-app/src/app/core/components/alert-container/alert-container.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'fin-alert-container',
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AlertContainerComponent {
-  @Input() message!: string;
+  public message = input.required<string>();
 }
