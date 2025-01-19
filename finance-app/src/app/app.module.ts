7c4ff1c3a33982b0b664e93adab8c5961a020d29
eduardo <eduardocardoso1039@gmail.com>
2025-01-19 12:11:41 -0300

    Feature | AppModule - Uso de providers

--- finance-app/src/app/app.module.ts
+++ finance-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 
 // modules
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // components
@@ -20,33 +20,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from '../environments/environment';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserAnimationsModule,
-    HttpClientModule,
-    BrowserModule,
-    RouterModule,
-    AppRoutingModule,
-    ToastModule,
-    ConfirmDialogModule,
-    AuthModule.forRoot({
-      domain: environment.auth0.domain,
-      clientId: environment.auth0.clientId,
-    }),
-    FontAwesomeModule,
-  ],
-  providers: [
-    MessageService,
-    ConfirmationService,
-    DialogService,
-    httpInterceptorProvider,
-    provideHttpClient(
-      withFetch()
-    )
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserAnimationsModule,
+        BrowserModule,
+        RouterModule,
+        AppRoutingModule,
+        ToastModule,
+        ConfirmDialogModule,
+        AuthModule.forRoot({
+            domain: environment.auth0.domain,
+            clientId: environment.auth0.clientId,
+        }),
+        FontAwesomeModule], providers: [
+        MessageService,
+        ConfirmationService,
+        DialogService,
+        httpInterceptorProvider,
+        provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
