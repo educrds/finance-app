7411b580c2087ef63a6fe883f6e351f1cedb1140
eduardo <eduardocardoso1039@gmail.com>
2025-01-20 17:14:43 -0300

    Feature | Preference - Criado model para preferencias

---
+++ finance-app/src/app/core/models/Preference.ts
@@ -0,0 +1,8 @@
+export type Preference = {
+  prf_id: number | null;
+  saidas_por_categoria: number;
+  comparativo_mensal: number;
+  saidas_por_metodo: number;
+  entradas_por_categoria: number;
+  usr_id: number;
+}
