932cf1683e9119f4d7db1fae100def4180b5ece8
eduardo <eduardocardoso1039@gmail.com>
2025-02-01 18:01:09 -0300

    Refactor | CoreRoutes - Removido código não utilizado

--- finance-app/src/app/core/core-routes.ts
+++ finance-app/src/app/core/core-routes.ts
@@ -1,11 +1,8 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { CategoriasComponent } from './components/categorias/categorias.component';
 import { DespesasComponent } from './components/despesas/despesas.component';
 import { MainComponent } from './components/main/main.component';
 import { ReceitasComponent } from './components/receitas/receitas.component';
-import { HomeComponent } from './pages/home/home.component';
-import { authGuard } from '../authenticator/guards/auth.guard';
 
 
 export const CORE_ROUTES: Routes = [
@@ -15,24 +12,3 @@ export const CORE_ROUTES: Routes = [
   { path: 'categorias', component: CategoriasComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 ]
-
-// const routes: Routes = [
-//   {
-//     path: '',
-//     component: HomeComponent,
-//     canActivateChild: [authGuard],
-//     children: [
-//       { path: 'dashboard', component: MainComponent },
-//       { path: 'entradas', component: ReceitasComponent },
-//       { path: 'saidas', component: DespesasComponent },
-//       { path: 'categorias', component: CategoriasComponent },
-//       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-//     ],
-//   },
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forChild(routes)],
-//   exports: [RouterModule]
-// })
-// export class CoreRoutingModule { }
