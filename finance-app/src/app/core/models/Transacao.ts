9829ce5a933bb24423854eca18fb4e62a1fcc8fd
eduardo <eduardocardoso1039@gmail.com>
2025-02-01 19:57:03 -0300

    Feature | Transacao - Adicionado model de form

--- finance-app/src/app/core/models/Transacao.ts
+++ finance-app/src/app/core/models/Transacao.ts
@@ -1,5 +1,6 @@
 export type Transacao = {
-  trs_data_ocorrido: string;
+  trs_data_ocorrido: string | Date;
+  data_fm: string | Date;
   trs_ano_ocorrido: number;
   trs_mes_ocorrido: number;
   trs_id: number;
@@ -10,11 +11,24 @@ export type Transacao = {
   categoria_cor: string;
   id_categoria: number;
   metodo_nome: string;
+  trs_metodo: string;
   metodo_id: number;
   tipo_transacao: string;
   id_tipo_transacao: number;
-  trs_parcelado: number;
+  trs_parcelado: number | boolean;
   par_id: number | null;
   trs_num_parcela: number | null;
   total_parcelas: number | null;
 };
+
+export type TransacaoForm = {
+  trs_valor: number,
+  trs_data_ocorrido: Date,
+  trs_titulo: string,
+  trs_categoria: string,
+  trs_usuario: string,
+  trs_metodo: string,
+  trs_status: boolean,
+  trs_parcelado: boolean,
+  data_fim_repeticao: string,
+}
