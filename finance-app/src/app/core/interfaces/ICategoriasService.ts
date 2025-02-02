6bde9007170e349ca28df96b978424b1718fbf7f
eduardo <eduardocardoso1039@gmail.com>
2025-02-02 19:56:33 -0300

    Refactor | ICategoriaService - Alterado interface da service

--- finance-app/src/app/core/interfaces/ICategoriasService.ts
+++ finance-app/src/app/core/interfaces/ICategoriasService.ts
@@ -6,6 +6,6 @@ export interface ICategoriasService {
   getCategoriasDropdown$(cat_tip_id: number): Observable<IDropdown[]>;
   getCategoriasByUser$(): Observable<Categorias[]>;
   addCategoria$(form: Categoria): Observable<{ message: string }>;
-  atualizarCategoria$(form: Categoria): Observable<{ message: string }>;
-  deletarCategoria$(form: Categoria): Observable<{ message: string }>;
+  atualizarCategoria$(form: Categorias): Observable<{ message: string }>;
+  deletarCategoria$(form: Categorias): Observable<{ message: string }>;
 }
