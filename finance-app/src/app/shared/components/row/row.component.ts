4f2c27455760f3f2dfea05bca1658cc7bfa36721
eduardo <eduardocardoso1039@gmail.com>
2025-02-15 17:00:36 -0300

    Styles | Row - Adicionar layout responsivo: tablet, celular e desktop

--- finance-app/src/app/shared/components/row/row.component.ts
+++ finance-app/src/app/shared/components/row/row.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'fin-row',
     templateUrl: './row.component.html',
+    styleUrl: "./row.component.scss",
     standalone: true,
 })
 export class RowComponent {
